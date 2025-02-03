6459082370742201cda12cf8c05a3b5962588cbf
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-03 11:49:36 -0300

    Initial Commit

---
+++ Wu-Tang2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wu-Tang2
+//
+//  Created by Christian Manzaraz on 03/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wu-Tang2/Wu_Tang2App.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_Tang2App.swift
+//  Wu-Tang2
+//
+//  Created by Christian Manzaraz on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Wu_Tang2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
